92a0b230216eb4c527f31b0604783a9dad9cb946
NotFrenk <franciscoleonfelici@gmail.com>
2025-02-26 15:10:20 +0100

    uni

---
+++ java_4/SpringUniversità/src/main/java/com/spring/university/dao/StudentiDAOMappaImpl.java
@@ -0,0 +1,41 @@
+package com.spring.university.dao;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.spring.university.entity.Studente;
+
+@Repository
+public class StudentiDAOMappaImpl {
+
+	private Map<Integer, Studente> mappa = new HashMap<>();
+
+	public boolean insert(Studente studente) {
+	if(mappa.containsKey(studente.getMatricola()))
+	return false;
+
+	mappa.put(studente.getMatricola(), studente);
+	return true;
+	}
+
+	public List<Studente> selectAll(){
+	return new ArrayList<>(mappa.values());
+	}
+
+	public Studente selectByMatricola(Integer idStudente) {
+	return mappa.get(idStudente);
+	}
+
+	public boolean delete(Integer idStudente) {
+	Studente studente = mappa.remove(idStudente);
+	return studente != null;
+	}
+
+
+	}
+
